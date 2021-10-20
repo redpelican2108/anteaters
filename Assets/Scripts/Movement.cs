29df1ab08775a74231171a766323d79997e5c811
redpelican2108 <70840956+redpelican2108@users.noreply.github.com>
2021-10-21 00:09:36 +0800

    Changed to rigidbody

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed;
+    private Rigidbody2D rb;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        transform.position += new Vector3(horizontal, vertical, 0).normalized * speed * Time.deltaTime;
-
+    { 
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position.normalized;
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
         
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(horizontal, vertical).normalized * speed * Time.deltaTime; 
     }
 }
